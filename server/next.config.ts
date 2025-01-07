d3b5c8caedb5784924d606e6e3bcb636d9262206
michihiro.goto <michihiro.goto@techpilot.net>
2025-01-07 21:23:42 +0100

    fetch flickr data

--- server/next.config.ts
+++ server/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        domains: ["live.staticflickr.com"],
+    },
   /* config options here */
 };
 
---
+++ server/src/utils/fetch-flickr-data.ts
@@ -0,0 +1,47 @@
+type Photo = {
+    id: string;
+    title: string;
+    farm: number;
+    server: string;
+    secret: string;
+};
+
+type FlickrResponse = {
+    photos: {
+        photo: Photo[];
+    };
+};
+
+export const getPhotoUrl = (photo: Photo, size: string = "m") =>  `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`;
+
+
+export const fetchFlickrData = async():Promise<FlickrResponse> => {
+
+    const {
+        FLICKR_API_KEY,
+        FLICKR_USER_ID,FLICKR_API_URL} = process.env
+
+    if (!
+        FLICKR_API_KEY || !
+        FLICKR_USER_ID) {
+        throw new Error("Missing API key or User ID");
+    }
+
+    const params = new URLSearchParams({
+        method: "flickr.people.getPhotos",
+        api_key:
+        FLICKR_API_KEY,
+        user_id:
+        FLICKR_USER_ID,
+        format: "json",
+        nojsoncallback: "1",
+    });
+
+    const response = await fetch(`${FLICKR_API_URL}?${params.toString()}`);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch photos: ${response.statusText}`);
+    }
+    const data = await response.json();
+
+    return data;
+}
