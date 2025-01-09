10d0993b89d8ec91ee5fd390405f088da7881aeb
michihiro.goto <michihiro.goto@techpilot.net>
2025-01-09 15:53:32 +0100

    SSG works for flickr images

--- server/src/app/album/page.tsx
+++ server/src/app/album/page.tsx
@@ -1,31 +1,30 @@
-import {fetchFlickrData, getPhotoUrl} from "@/utils/fetch-flickr-data";
+import { fetchFlickrData, getPhotoUrl } from "@/utils/fetch-flickr-data";
 import Image from "next/image";
 
-
 export default async function Album() {
-    const flickrData = await fetchFlickrData();
+  const flickrData = await fetchFlickrData();
 
-    return (
-        <div className="max-w-screen-lg mx-auto p-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {flickrData.photos.photo.map((data: any, index: number) => {
-                    const url = getPhotoUrl(data, "w");
-                    return (
-                        <div
-                            key={index}
-                            className="relative overflow-hidden rounded-lg shadow-lg transition-opacity duration-300 ease-in-out hover:opacity-100 opacity-80"
-                        >
-                            <Image
-                                src={url}
-                                alt=""
-                                width={300}
-                                height={200}
-                                className="object-cover w-full h-full"
-                            />
-                        </div>
-                    );
-                })}
+  return (
+    <div className="max-w-screen-lg mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {flickrData.photos.photo.map((data, index: number) => {
+          const url = getPhotoUrl(data, "w");
+          return (
+            <div
+              key={index}
+              className="relative overflow-hidden rounded-lg shadow-lg transition-opacity duration-300 ease-in-out hover:opacity-100 opacity-80"
+            >
+              <Image
+                src={url}
+                alt=""
+                width={300}
+                height={200}
+                className="object-cover w-full h-full"
+              />
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- server/src/utils/fetch-flickr-data.ts
+++ server/src/utils/fetch-flickr-data.ts
@@ -1,47 +1,42 @@
 type Photo = {
-    id: string;
-    title: string;
-    farm: number;
-    server: string;
-    secret: string;
+  id: string;
+  title: string;
+  farm: number;
+  server: string;
+  secret: string;
 };
 
-type FlickrResponse = {
-    photos: {
-        photo: Photo[];
-    };
+export type FlickrResponse = {
+  photos: {
+    photo: Photo[];
+  };
 };
 
-export const getPhotoUrl = (photo: Photo, size: string = "z" ) =>  `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
+export const getPhotoUrl = (photo: Photo, size: string = "z") =>
+  `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
 
+export const fetchFlickrData = async (): Promise<FlickrResponse> => {
+  const { FLICKR_API_KEY, FLICKR_USER_ID, FLICKR_API_URL } = process.env;
 
-export const fetchFlickrData = async():Promise<FlickrResponse> => {
+  if (!FLICKR_API_KEY || !FLICKR_USER_ID) {
+    throw new Error("Missing API key or User ID");
+  }
 
-    const {
-        FLICKR_API_KEY,
-        FLICKR_USER_ID,FLICKR_API_URL} = process.env
+  const params = new URLSearchParams({
+    method: "flickr.people.getPhotos",
+    api_key: FLICKR_API_KEY,
+    user_id: FLICKR_USER_ID,
+    format: "json",
+    nojsoncallback: "1",
+  });
 
-    if (!
-        FLICKR_API_KEY || !
-        FLICKR_USER_ID) {
-        throw new Error("Missing API key or User ID");
-    }
+  const response = await fetch(`${FLICKR_API_URL}?${params.toString()}`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch photos: ${response.statusText}`);
+  }
+  const data = await response.json();
 
-    const params = new URLSearchParams({
-        method: "flickr.people.getPhotos",
-        api_key:
-        FLICKR_API_KEY,
-        user_id:
-        FLICKR_USER_ID,
-        format: "json",
-        nojsoncallback: "1",
-    });
+  console.log(data);
 
-    const response = await fetch(`${FLICKR_API_URL}?${params.toString()}`);
-    if (!response.ok) {
-        throw new Error(`Failed to fetch photos: ${response.statusText}`);
-    }
-    const data = await response.json();
-
-    return data;
-}
+  return data;
+};
