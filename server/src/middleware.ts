0666f1fd7638300352399532825369a39a3825c4
michihiro.goto <michihiro.goto@techpilot.net>
2025-01-26 20:39:36 +0100

    test3

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -1,31 +1,37 @@
 import createMiddleware from "next-intl/middleware";
-import { defaultLocale, locales, routing } from "./i18n/routing";
-import { NextRequest, NextResponse } from "next/server";
+import { routing } from "./i18n/routing";
 
-export function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
-
-  const localeMatch = locales.find((locale) =>
-    pathname.startsWith(`/${locale}`),
-  );
-
-  if (!localeMatch) {
-    // ロケールが含まれていない場合、デフォルトのロケールを追加してリダイレクト
-    const url = new URL(`/${defaultLocale}${pathname}`, req.url);
-    return NextResponse.redirect(url);
-  }
-
-  const handleI18nRouting = createMiddleware({
-    ...routing,
-    alternateLinks: true,
-  });
-
-  const response = handleI18nRouting(req);
-
-  return response;
-}
+export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(en|jp)/:path*"],
+  matcher: ["/", "/(jp|en)/:path*"],
 };
+
+// export function middleware(req: NextRequest) {
+//   const pathname = req.nextUrl.pathname;
+//
+//   const localeMatch = locales.find((locale) =>
+//     pathname.startsWith(`/${locale}`),
+//   );
+//
+//   if (!localeMatch) {
+//     // ロケールが含まれていない場合、デフォルトのロケールを追加してリダイレクト
+//     const url = new URL(`/${defaultLocale}${pathname}`, req.url);
+//     return NextResponse.redirect(url);
+//   }
+//
+//   const handleI18nRouting = createMiddleware({
+//     ...routing,
+//     alternateLinks: true,
+//   });
+//
+//   const response = handleI18nRouting(req);
+//
+//   return response;
+// }
+//
+// export const config = {
+//   // Match only internationalized pathnames
+//   matcher: ["/", "/(en|jp)/:path*"],
+// };
