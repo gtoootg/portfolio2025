f0faf3ec0db58d7011ffa7632ddaf1d4823cb358
michihiro.goto <michihiro.goto@techpilot.net>
2025-05-22 11:42:54 +0200

    removed cn.ts

--- server/src/utils/cn.ts
+++
@@ -1,8 +0,0 @@
-import { ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...args: ClassValue[]) {
-    // see https://github.com/dcastil/tailwind-merge
-    // see https://github.com/lukeed/clsx
-    return twMerge(clsx(args))
-  }
