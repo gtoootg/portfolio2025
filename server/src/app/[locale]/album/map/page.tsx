277154522ef454679dafb1fd78c491b35edf9c1c
michihiro.goto <michihiro.goto@techpilot.net>
2025-06-07 12:37:02 +0200

    added light box

--- server/src/app/[locale]/album/map/page.tsx
+++ server/src/app/[locale]/album/map/page.tsx
@@ -1,30 +1,28 @@
 import { fetchFlickrPhotos, getPhotoUrl } from "@/api/flickr-api";
-import GoogleMapCluster from "@/components/google-map/google-map-cluster";
+import AlbumMap, {
+  AlbumMapMarkerProps,
+} from "@/components/google-map/album-map";
 
 export default async function AlbumMapPage() {
   const flickrData = await fetchFlickrPhotos();
   const center = { lat: 50.941278, lng: 6.958281 };
 
-  const markers = flickrData.photos.photo.reduce<
-    { lat: number; lng: number; key: string; imageUrl?: string }[]
-  >((acc, photo) => {
-    const lat = photo.latitude;
-    const lng = photo.longitude;
-
-    if (lat && lng) {
-      acc.push({
-        lat: Number(lat),
-        lng: Number(lng),
-        key: photo.id,
-        imageUrl: getPhotoUrl(photo, "w"),
-      });
-    }
-    return acc;
-  }, []);
-
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <GoogleMapCluster center={center} zoom={3} markers={markers} />
-    </div>
+  const markers = flickrData.photos.photo.reduce<AlbumMapMarkerProps[]>(
+    (acc, photo) => {
+      const lat = photo.latitude;
+      const lng = photo.longitude;
+      if (lat && lng) {
+        acc.push({
+          lat: Number(lat),
+          lng: Number(lng),
+          key: photo.id,
+          imageUrl: getPhotoUrl(photo, "w"),
+        });
+      }
+      return acc;
+    },
+    []
   );
+
+  return <AlbumMap center={center} markers={markers} />;
 }
--- server/src/components/google-map/google-map-cluster.tsx
+++ server/src/components/google-map/google-map-cluster.tsx
@@ -2,43 +2,50 @@
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
+export interface GoogleMapCluserMarkerProps {
+  lat: number;
+  lng: number;
+  key?: string | number;
+  imageUrl?: string;
+}
+
 export interface GoogleMapClusterProps {
   center: { lat: number; lng: number };
   zoom: number;
-  markers: {
-    lat: number;
-    lng: number;
-    key?: string | number;
-    imageUrl?: string;
-  }[];
+  markers: GoogleMapCluserMarkerProps[];
+  onMarkerClick?: (marker: GoogleMapCluserMarkerProps) => void;
 }
 
 export default function GoogleMapCluster({
   center,
   zoom,
   markers,
+  onMarkerClick,
 }: GoogleMapClusterProps) {
   const { GOOGLE_MAP_API_KEY } = process.env;
 
   // GoogleMapのonLoadでクラスタリングをセットアップ
   const handleOnLoad = (map: google.maps.Map) => {
     if (!markers.length) return;
-    const gMarkers = markers.map(
-      (marker) =>
-        new window.google.maps.Marker({
-          position: { lat: marker.lat, lng: marker.lng },
-          map,
-          ...(marker.imageUrl
-            ? {
-                icon: {
-                  url: marker.imageUrl,
-                  scaledSize: new window.google.maps.Size(48, 48),
-                  anchor: new window.google.maps.Point(24, 24),
-                },
-              }
-            : {}),
-        })
-    );
+    const gMarkers = markers.map((marker) => {
+      const gMarker = new window.google.maps.Marker({
+        position: { lat: marker.lat, lng: marker.lng },
+        map,
+        ...(marker.imageUrl
+          ? {
+              icon: {
+                url: marker.imageUrl,
+                scaledSize: new window.google.maps.Size(48, 48),
+                anchor: new window.google.maps.Point(24, 24),
+              },
+            }
+          : {}),
+      });
+      if (onMarkerClick) {
+        gMarker.addListener("click", () => onMarkerClick(marker));
+      }
+      return gMarker;
+    });
     new MarkerClusterer({ markers: gMarkers, map });
   };
 
