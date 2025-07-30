62f978a81e973cfde63c9ad424758a7444bf15fc
michihiro.goto <michihiro.goto@techpilot.net>
2025-07-30 09:01:32 +0200

    changed album map to client component

--- server/src/app/[locale]/album/map/page.tsx
+++ server/src/app/[locale]/album/map/page.tsx
@@ -1,30 +1,41 @@
-import { fetchFlickrPhotos, getPhotoUrl } from "@/api/flickr-api";
+"use client";
+
+import { getPhotoUrl } from "@/api/flickr-api";
+import { useFlickrPhotos } from "@/api/use-flickr-photos.hooks";
+import { LoadingProgress } from "@/components/atoms/loading-progress.tsx/loading-progress";
 import AlbumMap, {
   AlbumMapMarkerProps,
 } from "@/components/google-map/album-map/album-map";
+import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
 
-export default async function AlbumMapPage() {
-  const t = await getTranslations("/album");
-  const flickrData = await fetchFlickrPhotos();
+export default function AlbumMapPage() {
+  const t = useTranslations("/album");
   const center = { lat: 50.941278, lng: 6.958281 };
 
-  const markers = flickrData.photos.photo.reduce<AlbumMapMarkerProps[]>(
-    (acc, photo) => {
-      const lat = photo.latitude;
-      const lng = photo.longitude;
-      if (lat && lng) {
-        acc.push({
-          lat: Number(lat),
-          lng: Number(lng),
-          key: photo.id,
-          imageUrl: getPhotoUrl(photo, "w"),
-        });
-      }
-      return acc;
-    },
-    []
-  );
+  const { data, isLoading } = useFlickrPhotos();
+
+  const markers = data?.reduce<AlbumMapMarkerProps[]>((acc, photo) => {
+    const lat = photo.latitude;
+    const lng = photo.longitude;
+    if (lat && lng) {
+      acc.push({
+        lat: Number(lat),
+        lng: Number(lng),
+        key: photo.id,
+        imageUrl: getPhotoUrl(photo, "w"),
+      });
+    }
+    return acc;
+  }, []);
+
+  if (isLoading) {
+    return <LoadingProgress />;
+  }
+
+  if (!markers || markers.length === 0) {
+    return;
+  }
 
   return (
     <AlbumMap
